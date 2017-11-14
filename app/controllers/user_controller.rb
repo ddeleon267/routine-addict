746ff4a96ddc06f9ca2e90217cd2adbb17447c3f
github username <github email address>
2017-11-14 16:11:19 -0500

    Added preliminary /signup controller action and notes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
+########### SIGNUP #############
+
+  get '/signup' do
+    #if the user is logged in, take them to their hom page. otherwise, direct to signup
+    #page ----> need helper method for whether a user is logged in.
+    erb :'/users/signup'
+  end
+
 
 end
