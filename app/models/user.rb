51020d46880c669a875beeca2368330b259f804f
github username <github email address>
2017-11-15 12:00:20 -0500

    Added methods to user class to slug username, find user object by slug, and display current user by session id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :routines
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|user| user.slug == slug}
+  end
+
+  def self.current_user(session)
+    self.find(session[:id])
+  end
 end
