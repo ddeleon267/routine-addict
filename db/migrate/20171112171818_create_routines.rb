c02a7b94b4388ba12b40a76984c4c75bd8acf63c
github username <github email address>
2017-11-15 06:14:12 -0500

    Modified/simplified migrations for tables. Can always add columns later.' '

--- db/migrate/20171112171818_create_routines.rb
+++ db/migrate/20171112171818_create_routines.rb
@@ -2,10 +2,8 @@ class CreateRoutines < ActiveRecord::Migration[5.1]
   def change
     create_table :routines do |t|
       t.string :name
-      t.string :time_of_day
-      t.string :season
-      t.string :climate
-      t.string :products
+      t.text :products
+      t.text :description
       t.integer :user_id
     end
 end
