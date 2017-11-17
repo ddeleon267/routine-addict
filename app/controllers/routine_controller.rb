f4ddb6f9acf63dfb63bb5b26f7f2dd6ec67f085a
github username <github email address>
2017-11-17 14:01:40 -0500

    Added create routes for routine controller.Both include validation that user is logged in and redirect appropriately. Routine index successfully displays list of all tweets, and routine show page displays the attributes of a given routine

--- app/controllers/routine_controller.rb
+++ app/controllers/routine_controller.rb
@@ -1,5 +1,6 @@
 class RoutineController < ApplicationController
 
+  ############ CREATE ###########
   get '/routines/new' do
     if logged_in?(session)
       erb :'routines/new'
@@ -10,10 +11,10 @@ class RoutineController < ApplicationController
 
   post '/routines' do
     # binding.pry
-    if params[:name] != ""
+    if params[:name] != "" && params[:products] != []
       routine = Routine.new(
         name: params[:name],
-        products: params[:products].split(",").collect {|name| Product.new(name: name)},
+        products: params[:products].split(",").collect {|name| Product.create(name: name)},
         description: params[:description]
         )
       routine.user_id = session[:id]
@@ -21,5 +22,24 @@ class RoutineController < ApplicationController
       redirect to '/routines'
     end
     redirect to '/routines/new'
+  end
+
+  ############ READ ###########
+  get '/routines' do
+    @routines = Routine.all  ###this is going to need to be modified
+    if logged_in?(session)
+      erb :'/routines/index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/routines/:id' do
+    if logged_in?(session)
+      @routine = Routine.find(params[:id])
+      erb :'/routines/show'
+    else
+      redirect to '/login'
+    end
   end
 end
