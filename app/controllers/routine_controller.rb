f24659d0ac458084594dad601e432b403f3b7e9e
github username <github email address>
2017-11-17 13:35:01 -0500

    Added contoller routes for routine /new action, plus a preliminary form that seems to work on the routine /new view

--- app/controllers/routine_controller.rb
+++ app/controllers/routine_controller.rb
@@ -1,3 +1,25 @@
 class RoutineController < ApplicationController
 
+  get '/routines/new' do
+    if logged_in?(session)
+      erb :'routines/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/routines' do
+    # binding.pry
+    if params[:name] != ""
+      routine = Routine.new(
+        name: params[:name],
+        products: params[:products].split(",").collect {|name| Product.new(name: name)},
+        description: params[:description]
+        )
+      routine.user_id = session[:id]
+      routine.save
+      redirect to '/routines'
+    end
+    redirect to '/routines/new'
+  end
 end
