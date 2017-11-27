a510378ab275397cfda27d2df17a8c23735d5bca
github username <github email address>
2017-11-27 15:24:43 -0500

    Updated users/:slug/main controller action so that a logged in user can only see his own main page, and not that of any other user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
 ########### SIGNUP #############
 
 get '/signup' do
-  # if the user is logged in, take them to their hom page. otherwise, direct to signup
-  # page ----> need helper method for whether a user is logged in.
   if !logged_in?
     erb :'/users/signup'
   else
@@ -21,67 +19,61 @@ post '/signup' do
   else
     redirect to '/signup'
   end
-  #redirect somewhere.... like page that lists all routines??
-  #otherwise...redirect to signup page or do flash message
 end
-#
-#   ############ LOGIN ############
-#
+
+############ LOGIN ############
+
   get '/login' do
-    #if user not logged in, show login form
     if !logged_in?
       erb :'users/login'
     else
       redirect to "/users/#{current_user.slug}/main"
-    #otherwise redirect to their main page, whatever that is
     end
   end
-#
+
   post '/login' do
-    #find user
     @user = User.find_by(username: params[:username])
+    #if user exists and pasword matches
     if @user && @user.authenticate(params[:password])
       session[:id] = @user.id
       redirect to "/users/#{@user.slug}/main"
     else
       redirect to '/signup'
-      #will probably need to adapt... ?? what if exist but password bad??
     end
-    #if user exists and pasword matches, set session id and redirect to main page
-    #otherwise redirect back to signup
   end
-#
-#   ############ SHOW USER ############
-  get '/users/:slug/main' do
+
+############ SHOW USER ############
+  get '/users/:slug/main' do ########
 
     @user = User.find_by_slug(params[:slug])
-    #users show page --> only user can see this page --> kind of like your FB home page
+    #users main page --> only user can see this page --> kind of like your FB home page
 
-    if logged_in?
+    if logged_in? && current_user.id == @user.id
       @routines = Routine.all
       @products = Product.all
       erb :'/users/main'
+    elsif  logged_in? && current_user.id != @user.id
+      redirect to "/users/#{current_user.slug}/main"
       #will need to add something to make sure current user is right
     else
       redirect to '/login'
     end
   end
-#
-#
+
   get '/users/:slug' do
     #user show page ---> for other users and owner to see ---> kind of like FB timeline
     if logged_in?
 
       @user = User.find_by_slug(params[:slug])
-      @user_routines = @user.routines 
+      @user_routines = @user.routines
       erb :'/users/show'
     else
       redirect to '/login'
     end
   end
-#
-#   ############ LOGOUT ############
-#
+
+############ LOGOUT ############
+
   get '/logout' do
     #if logged in, clear session, redirect to login
     #otherwise, redirect to main index page. could change these redirects as well
