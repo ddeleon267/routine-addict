19137406699c9d876b05877596a66728aef54e14
github username <github email address>
2017-11-28 13:31:17 -0500

    Added form to add new product, w/ correctly structured params hash. Added post '/products' action and preliminary get '/posts' action that displays all product names

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,30 @@ class ProductsController < ApplicationController
     end
   end
 
+  post '/products' do
+    if !params[:product][:name].empty?
+      if Product.find_by(name: params[:product][:name])
+        redirect to '/products/new'
+        #flash message -- this product exitss
+      else
+        product = Product.create(
+          name: params[:product][:name],
+          category: params[:product][:category],
+          ingredients: params[:product][:ingredients],
+          notes: params[:product][:notes],
+          )
+          binding.pry
+        redirect '/products'
+      end
+    else params[:product][:name].empty?
+      #flash message
+      redirect '/products/new'
+    end
+  end
+
+  get '/products' do
+    @products = Product.all
+    erb :'products/index'
+  end
+
 end
