d943e315bf4300e7ea04a9a594dcdc3b2700c439
github username <github email address>
2018-01-30 18:52:15 -0500

    Can successfully edit comments. Only the user associated with a comment can edit or delete the comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,39 @@
 class CommentsController < ApplicationController
 
   post '/comments' do
-    if logged_in? && !params[:comment][:content].empty?
-      @comment = Comment.new(params[:comment])
-      @routine = Routine.find(params[:comment][:routine_id])
+    @comment = Comment.new(params[:comment])
+    @routine = Routine.find(params[:comment][:routine_id])
 
+    if logged_in? && !params[:comment][:content].empty?
       @comment.user_id = session[:id]
       @comment.routine_id = @routine.id
-
       @comment.save
-    else
-      flash.next[:message] = "Comments cannot be blank"
     end
     redirect to "/routines/#{@routine.id}"
   end
 
+  get '/comments/:id/edit' do
+    @comment = Comment.find(params[:id])
+    if logged_in? && current_user.id == @comment.user_id
+      erb :'/comments/edit'
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/comments/:id' do
+    @comment = Comment.find(params[:id])
+    if logged_in? && current_user.id == @comment.user_id
+      @comment.update(params[:comment])
+      @comment.save
+    else
+      flash.next[:message] = "Comments cannot be blank"
+    end
+    redirect to "/routines/#{@comment.routine_id}"
+  end
+
   delete '/comments/:id/delete' do
     @comment = Comment.find(params[:id])
-
 
     if logged_in? && current_user.id == @comment.user_id
       @comment.delete
